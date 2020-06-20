d75b33891642c382d8ac507158d1d26720104b64
Mauricio Robayo <rfmajo@gmail.com>
2020-06-20 07:21:28 -0500

    chore: initial state setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+interface Props {
+}
+
+interface State {
+  basics:  Basics
+}
+
+interface Basics {
+  name: string;
+  website: string;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+        basics: {
+          name: "Mauricio Robayo",
+          website: "https://www.mauriciorobayo.com"
+        }
+    };
+  }
+
+  render() {
+    return (<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <h1><a href={this.state.basics.website}>{this.state.basics.name}</a></h1>
       </header>
-    </div>
-  );
+    </div>)
+  }
 }
 
 export default App;
