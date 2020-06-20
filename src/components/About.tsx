95d0a0d53566b3b62d28bd6ad30dcf10a49a2ffe
Mauricio Robayo <rfmajo@gmail.com>
2020-06-20 11:59:58 -0500

    feat: add about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,13 @@
 import React, { FunctionComponent } from 'react';
+import { Basics } from '../types'
 
-const About: FunctionComponent = () => (
-  <div>Hello, world</div>
+interface Props {
+  isLoading: Boolean;
+  basics: Basics;
+}
+
+const About: FunctionComponent<Props> = ({basics: { summary }, isLoading }: Props) => (
+  <div>{summary?.split('\n').map(line => <p>{line}</p>)}</div>
 )
 
 export default About;
