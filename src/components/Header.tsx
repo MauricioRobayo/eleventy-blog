f23498f2421f4d3a946f03563273eaa475d80152
Mauricio Robayo <rfmajo@gmail.com>
2020-06-20 11:36:58 -0500

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import Profile from './Profiles';
+import { Basics } from  '../types';
+
+interface Props {
+  basics: Basics;
+  isLoading: Boolean;
+}
+
+const Header: FunctionComponent<Props> = ({ basics: {website, name, profiles}, isLoading }: Props) => (
+  <header className={`App-header ${isLoading ? 'loading' : ''}`}>
+    <h1><a href={website}>{name}</a></h1>
+    <Profile profiles={profiles}></Profile>
+  </header>
+)
+
+export default Header;
---
+++ src/components/Profiles.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent } from "react";
+import { Profile } from "../types";
+import ProfileItem from "./ProfileItem";
+
+interface Props {
+  profiles: Profile[] | undefined;
+}
+
+const displayedProfiles: string[] = ["github", "linkedin", "twitter"];
+
+const Profiles: FunctionComponent<Props> = ({ profiles }: Props) => (
+  <ul>
+    {profiles
+      ?.filter((profile: Profile) =>
+        displayedProfiles.includes(profile.network.toLowerCase())
+      )
+      .map((profile: Profile) => (
+        <ProfileItem {...profile}></ProfileItem>
+      ))}
+  </ul>
+);
+
+export default Profiles;
