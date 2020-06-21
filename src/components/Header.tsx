b09638b763bcd16595a951321cbe008188d258f3
Mauricio Robayo <rfmajo@gmail.com>
2020-06-21 11:41:06 -0500

    styles: add header css module

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import Profile from './Profiles';
 import { Basics } from  '../types';
+import styles from './Header.module.css';
 
 interface Props extends Basics {
   isLoading?: Boolean;
 }
 
-const Header: FunctionComponent<Props> = ({ website, name, profiles, isLoading = false }: Props) => (
-  <header className={`App-header ${isLoading ? 'loading' : ''}`}>
-    <h1><a href={website}>{name}</a></h1>
+const Header: FunctionComponent<Props> = ({ name, profiles, isLoading = false }: Props) => (
+  <header className={styles.header}>
+    <h1 className={styles.title}>{name}</h1>
     <Profile profiles={profiles}></Profile>
   </header>
 )
