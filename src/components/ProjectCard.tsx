2519e301b85526c2b3ed059345a17965b2eb077d
Mauricio Robayo <rfmajo@gmail.com>
2020-06-22 10:41:58 -0500

    styles: add project card styles

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,10 +2,10 @@ import React, { FunctionComponent } from 'react';
 import { Project } from '../types';
 import styles from './ProjectCard.module.css';
 
-const ProjectCard: FunctionComponent<Project> = ({name, summary, githubUrl, website}: Project) => (
+const ProjectCard: FunctionComponent<Project> = ({name, summary, githubUrl}: Project) => (
   <article className={styles.projectCard}>
     <h3>
-      {name.replace(/-/g, ' ')}
+      <a href={githubUrl}>{name.replace(/-/g, ' ')}</a>
     </h3>
     <p>
       {summary}
