f9410219c4e9f6bdd41abd2f1455b8773b00f2a1
Mauricio Robayo <rfmajo@gmail.com>
2020-06-21 20:33:12 -0500

    feat: add full information to project cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,8 +1,30 @@
 import React, { FunctionComponent } from 'react';
 import { Project } from '../types';
 
-const ProjectCard: FunctionComponent<Project> = (project: Project) => (
-  <li>{project.name}</li>
+const ProjectCard: FunctionComponent<Project> = ({displayName, summary, languages, githubUrl, website}: Project) => (
+  <article>
+    <h2>
+      {displayName.toUpperCase()}
+    </h2>
+    <p>
+      {summary}
+    </p>
+    <aside>
+      <ul>
+        {languages.map((language) => <li key={language}>{language}</li> )}
+      </ul>
+      <a href={githubUrl}>
+        <svg>
+          <use href="#logo-github"></use>
+        </svg>
+      </a>
+      {website && <a href={website}>
+        <svg>
+          <use href="#logo-www"></use>
+        </svg>
+        </a>}
+    </aside>
+  </article>
 )
 
 export default ProjectCard;
