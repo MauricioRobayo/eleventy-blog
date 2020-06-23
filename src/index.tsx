077c6a2ce738ab8530209496eac7468d069ac516
Mauricio Robayo <rfmajo@gmail.com>
2020-06-23 09:44:58 -0500

    chore: use webfontloader

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['IBM Plex Mono:400,700', 'IBM Plex Sans:400,700']
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
