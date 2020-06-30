9bf8f8ca865c80f199591f03646c8fdbbbf97d11
Mauricio Robayo <rfmajo@gmail.com>
2020-06-30 12:25:18 -0500

    test: loading and loaded tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
 
 jest.mock('./utils/portfolio');
 
-test('renders loading app while fetching data', () => {
-  const { getByText, queryAllByRole, queryByRole } = render(<App />);
-  const header = getByText('Mauricio Robayo');
-  const listitems = queryAllByRole('listitem');
-  const navigation = queryByRole('navigation');
-  expect(header).toBeInTheDocument();
-  expect(header).toHaveClass('loading');
-  expect(listitems.length).toBe(0);
-  expect(navigation).toBe(null);
+test('portfolio loads and renders', async () => {
+  render(<App />);
+
+  const loadingHeading = screen.getByRole('heading', {
+    name: 'Mauricio Robayo',
+  });
+  expect(loadingHeading).toBeInTheDocument();
+  expect(loadingHeading).toHaveClass('loading');
+
+  const loadingNavigation = screen.queryByRole('navigation');
+  expect(loadingNavigation).toBe(null);
+
+  const loadingLists = screen.queryAllByRole('listitem');
+  expect(loadingLists.length).toBe(0);
+
+  const loadedHeading = await screen.findByRole('heading', {
+    name: 'Mauricio Robayo',
+  });
+  expect(loadedHeading).toBeInTheDocument();
+
+  const loadedLists = await screen.findAllByRole('listitem');
+  expect(loadedLists.length).toBe(3);
+
+  const loadedNavigation = await screen.findByRole('navigation');
+  expect(loadedNavigation).toBeInTheDocument();
 });
 
-test('renders loaded app after fetching data', async () => {
-  const { getByText, findAllByRole, findByRole } = render(<App />);
-  const header = getByText('Mauricio Robayo');
-  const listitems = await findAllByRole('listitem');
-  const navigation = await findByRole('navigation');
-  expect(header).toBeInTheDocument();
-  expect(header).not.toHaveClass('loading');
-  expect(listitems.length).toBe(3);
-  expect(navigation).toBeInTheDocument();
-});
+test('an error is rendered if there is a problem getting course info', async () => {});
