efbf19854234fd7fc269b3c3122d96da8fe8671b
Mauricio Robayo <rfmajo@gmail.com>
2020-07-09 09:20:55 -0500

    test: inlcude all menu items

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,14 +12,16 @@ describe('Button', () => {
       <App apiUrl="https://gitconnected.com/v1/portfolio/mauriciorobayo" />
     );
 
+    // Before loading
+
     const loadingHeading = screen.getByRole('heading', {
       name: 'Mauricio Robayo',
     });
     expect(loadingHeading).toBeInTheDocument();
     expect(loadingHeading).toHaveClass('loading');
 
-    const loadingNavigation = screen.queryByRole('navigation');
-    expect(loadingNavigation).toBe(null);
+    const loadingMenu = screen.queryByRole('navigation');
+    expect(loadingMenu).toBe(null);
 
     const loadingProfiles = screen.queryByRole('list', { name: 'profiles' });
     expect(loadingProfiles).toBe(null);
@@ -27,7 +29,7 @@ describe('Button', () => {
     const loadingFooter = screen.queryByRole('contentinfo');
     expect(loadingFooter).toBe(null);
 
-    // Include all elements that should not be loaded
+    // After loading
 
     const loadedHeading = await screen.findByRole('heading', {
       name: 'Mauricio Robayo',
@@ -35,34 +37,54 @@ describe('Button', () => {
     expect(loadedHeading).toBeInTheDocument();
     expect(loadingHeading).not.toHaveClass('loading');
 
-    const profiles = await screen.findByRole('list', { name: 'profiles' });
-    expect(profiles).toBeInTheDocument();
+    const loadedProfiles = await screen.findByRole('list', {
+      name: 'profiles',
+    });
+    expect(loadedProfiles).toBeInTheDocument();
 
     const profileItems = await screen.findAllByRole('listitem');
     expect(profileItems.length).toBe(3);
-    profileItems.forEach((profile) => {
-      expect(profile.classList.contains('profileItem')).toBe(true);
+    profileItems.forEach((profileItem) => {
+      expect(profileItem).toBeInTheDocument();
+      expect(profileItem).toHaveClass('profileItem');
     });
 
-    const loadedNavigation = await screen.findByRole('navigation');
+    const loadedNavigation = await screen.getByRole('navigation');
     expect(loadedNavigation).toBeInTheDocument();
 
     const aboutMenuItem = await screen.findByRole('link', {
       name: 'About',
     });
+    expect(aboutMenuItem).toBeInTheDocument();
+    expect(aboutMenuItem).toHaveClass('selected');
+
     const projectsMenuItem = await screen.findByRole('link', {
       name: 'Projects',
     });
-
-    expect(aboutMenuItem).toBeInTheDocument();
     expect(projectsMenuItem).toBeInTheDocument();
-    expect(aboutMenuItem).toHaveClass('selected');
     expect(projectsMenuItem).not.toHaveClass('selected');
+
+    const contactMenuItem = await screen.findByRole('link', {
+      name: 'Contact',
+    });
+    expect(contactMenuItem).toBeInTheDocument();
+    expect(contactMenuItem).not.toHaveClass('selected');
+
+    const blogMenuItem = await screen.findByRole('link', {
+      name: 'Blog',
+    });
+    expect(blogMenuItem).toBeInTheDocument();
+    expect(blogMenuItem).not.toHaveClass('selected');
 
     fireEvent.click(projectsMenuItem);
 
     expect(aboutMenuItem).not.toHaveClass('selected');
     expect(projectsMenuItem).toHaveClass('selected');
+
+    fireEvent.click(contactMenuItem);
+
+    expect(projectsMenuItem).not.toHaveClass('selected');
+    expect(contactMenuItem).toHaveClass('selected');
 
     const loadedFooter = await screen.findByRole('contentinfo');
     expect(loadedFooter).toBeInTheDocument();
