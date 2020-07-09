3351cb121ecf6baed610c179d921ec54af6a38bf
Mauricio Robayo <rfmajo@gmail.com>
2020-07-09 11:34:56 -0500

    test: mock localStorage

--- src/utils/cache.test.ts
+++ src/utils/cache.test.ts
@@ -2,23 +2,54 @@ import Cache from './cache';
 import rawPortfolio from './__mocks__/gitconnectedMockData';
 import apiDataParser from './apiDataParser';
 
+const portfolio = apiDataParser(rawPortfolio);
+const getItemSpy = jest.spyOn(Storage.prototype, 'getItem');
+const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+const localStorageKey = 'test';
+
+const storeMock = (function () {
+  const store = {};
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+    setItem(key: string, value: any) {
+      store[key] = value;
+    },
+  };
+})();
+
+getItemSpy.mockImplementation(storeMock.getItem);
+setItemSpy.mockImplementation(storeMock.setItem);
+
+afterEach(() => {
+  getItemSpy.mockClear();
+  setItemSpy.mockClear();
+});
+
 describe('Cache', () => {
-  const portfolio = apiDataParser(rawPortfolio);
   it('should return null not updated', () => {
-    const cache = new Cache('test', 15);
+    const cache = new Cache(localStorageKey, 15);
     expect(cache.get()).toBe(null);
+    expect(getItemSpy).toBeCalledTimes(1);
+    expect(getItemSpy).toBeCalledWith(localStorageKey);
   });
 
   it('should update the cache', () => {
     const cache = new Cache('test', 15);
     cache.update(portfolio);
+    expect(setItemSpy).toBeCalledTimes(1);
     const { portfolio: cachedPortfolio, expiration } = cache.get();
     expect(cachedPortfolio).toEqual(portfolio);
     expect(expiration).toBeGreaterThan(Date.now());
+    expect(getItemSpy).toBeCalledTimes(1);
   });
+
   it('should return null not updated', () => {
     const cache = new Cache('test', -15);
     cache.update(portfolio);
+    expect(setItemSpy).toBeCalledTimes(1);
     expect(cache.get()).toBe(null);
+    expect(getItemSpy).toBeCalledTimes(1);
   });
 });
