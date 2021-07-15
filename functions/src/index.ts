20874a097c45ed0ad672fc4fce065b24ebd0d7ad
Mauricio Robayo <rfmajo@gmail.com>
2021-07-15 08:23:32 -0500

    feat: add repos api endpoint
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,46 @@
 import * as functions from "firebase-functions";
+import { Octokit } from "@octokit/rest";
+import * as admin from "firebase-admin";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const db = admin.firestore();
+const octokit = new Octokit();
+const CACHE_EXPIRATION_PERIOD = 24 * 60 * 60 * 1000;
+
+export const repos = functions.https.onRequest(async (_req, res) => {
+  const docRef = db.collection("cache").doc("repos");
+  const cached = await docRef.get();
+
+  if (
+    cached.updateTime &&
+    cached.updateTime.toMillis() > Date.now() - CACHE_EXPIRATION_PERIOD
+  ) {
+    functions.logger.log(
+      `Cache hit! Updated time ${cached.updateTime.toDate()}`
+    );
+    res.json(cached.data()?.repos);
+    return;
+  }
+
+  const { data } = await octokit.rest.search.repos({
+    q: "user:MauricioRobayo",
+    sort: "stars",
+  });
+
+  // functions.logger.log(headers);
+
+  const repos = data.items.map((repo) => ({
+    name: repo.name,
+    html_url: repo.html_url,
+    description: repo.description,
+    stargazers_count: repo.stargazers_count,
+    language: repo.language,
+  }));
+
+  await docRef.set({ repos });
+  functions.logger.log("Cache miss!");
+  functions.logger.log((await docRef.get()).updateTime?.toDate());
+
+  res.json(repos);
 });
